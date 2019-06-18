78600243572c7cae76304d13112a9ca86f023843
Umiker <jumiker@amazon.com>
2019-06-19 09:29:34 +1000

    Simplified the path routing support to Azure in app.js

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ const express = require('serverless-express/express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.get('/api/app', (req, res) => res.send('Hello World!'))
+// Azure's default URI path for this is /api/app and it sends that through so we need to cater for that
+app.get(['/','/api/app'], (req, res) => res.send('Hello World!'))
 
 //app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 module.exports = app
