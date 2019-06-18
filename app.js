8597df6489178408d89eacd0fe9b7f50b9f7346c
Umiker <jumiker@amazon.com>
2019-06-19 09:13:47 +1000

    Added Azure support

--- app.js
+++ app.js
@@ -5,5 +5,7 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get('/api/app', (req, res) => res.send('Hello World!'))
+
 //app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 module.exports = app
